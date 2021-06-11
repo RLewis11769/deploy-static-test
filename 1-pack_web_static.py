1937b80b6940f1875fc651520a5a8a4aa6d864ff
Rachel Lewis <RLewis11769@gmail.com>
2021-06-11 20:34:24 +0000

    Add returns

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,9 +7,15 @@ from datetime import datetime
 def do_pack():
     """ Pack up web_static """
 
-    now = datetime.now()
-    tarArchiveName = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
-    tarArchivePath = "versions/" + tarArchiveName
-    local("mkdir -p versions")
+    try:
+        now = datetime.now()
+        tarArchiveName = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+        tarArchivePath = "versions/" + tarArchiveName
 
-    local("tar -czvf " + tarArchivePath + " web_static")
+        local("mkdir -p versions")
+
+        local("tar -czvf " + tarArchivePath + " web_static")
+
+        return tarArchivePath
+    except:
+        return None
